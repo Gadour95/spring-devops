4d0e4eada244f130d093bd0a32af6d762376fced
Gaddour95 <abdelkadersniha@gmail.com>
2025-11-27 22:08:35 +0100

    First commit

---
+++ src/main/java/tn/esprit/spring/control/UserRestControlImpl.java
@@ -0,0 +1,17 @@
+package tn.esprit.spring.control;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestControlImpl {
+
+    // http://localhost:8083/SpringMVC/servlet/
+    @RequestMapping("/")
+    @ResponseBody
+    public String welcome() {
+        return "Hello from Abdelkader Sniha.";
+    }
+
+}
